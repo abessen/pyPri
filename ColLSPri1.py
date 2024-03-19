f7536d80b0349b5e896a006b8edece8ae9c57a00
Alan Bessen <abessen@quarryvision.com>
2024-03-19 08:29:09 -0500

    new commit

--- ColLSPri1.py
+++ ColLSPri1.py
@@ -46,7 +46,8 @@ def main():
 
     # Keep updating the image every 60 seconds
     while True:
-        time.sleep(60)
+     #   time.sleep(60)
+        
         # Update the image in-place
       #  updated_image = Image.open(image_path)
       #  image_container.image(updated_image, use_column_width=True)
