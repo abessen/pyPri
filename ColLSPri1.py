700f026c392d7c4a7df589b6f4459ea21c770c60
Alan Bessen <abessen@quarryvision.com>
2024-03-21 12:23:32 -0500

    new commit

--- ColLSPri1.py
+++ ColLSPri1.py
@@ -82,7 +82,7 @@ def main():
 
     
     # Rerun the Streamlit app after 30 seconds
-    st.experimental_rerun(interval=30)
+    st.experimental_rerun(30)
 
     # Wait for 30 seconds before the next iteration
     #time.sleep(30)  
