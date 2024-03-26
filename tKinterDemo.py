64f4d00efb2a453de7615f04ddb8283572b0d41c
Alan Bessen <abessen@quarryvision.com>
2024-03-26 11:28:48 -0500

    new commit

--- tKinterDemo.py
+++ tKinterDemo.py
@@ -34,7 +34,7 @@ label.place(x=0, y=0)  # Place the image at the top-left corner of the window
 
 # Create Dropdownbox
 start_combo = ttk.Combobox(root, values=HrSel)
-start_combo.place(x=100, y=10)  # Adjust the coordinates for indentation
+start_combo.place(x=140, y=12)  # Adjust the coordinates for indentation
 
 # Run the Tkinter event loop
 root.mainloop()
