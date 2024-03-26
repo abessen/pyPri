3ba2bcbb69edfeda2c5156f5d94b94df09b5445e
Alan Bessen <abessen@quarryvision.com>
2024-03-26 12:27:14 -0500

    new commit

--- tKinterDemo.py
+++ tKinterDemo.py
@@ -21,12 +21,10 @@ HrSel = ("12:01 AM", "12:30 AM", "1:00 AM", "1:30 AM", "2:00 AM", "2:30 AM", "3:
          "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM", "9:00 PM", "9:30 PM", "10:00 PM", "10:30 PM", "11:00 PM",
          "11:30 PM")
 
-
-RateSel = ("200", "250", "300", "350", "400", "450", "500", "550", "600", "650", "700", "750", "800", "850", "900", "950", "1000", "1050", "1100", "1150", "1200", "1250", "1300", "1350", "1400", "1450", "1500", "1550", "1600", "1650", "1700", "1750", "1800", "1850", "1900", "1950", "2000", "2050", "2100", "2150", "2200", "2250", "2300", "2400")
-
-
-
-
+RateSel = ("200", "250", "300", "350", "400", "450", "500", "550", "600", "650", "700", "750", "800", "850", "900",
+           "950", "1000", "1050", "1100", "1150", "1200", "1250", "1300", "1350", "1400", "1450", "1500", "1550",
+           "1600", "1650", "1700", "1750", "1800", "1850", "1900", "1950", "2000", "2050", "2100", "2150", "2200",
+           "2250", "2300", "2400")
 
 # Load the image
 image_path = r"C:\pyPri\ColLSToday2.jpg"
@@ -51,6 +49,10 @@ start_combo1.place(x=134, y=14)  # Adjust the coordinates for indentation
 # Create the second Dropdownbox (placed 100px to the right of the first one)
 start_combo2 = ttk.Combobox(root, values=HrSel, width=9, height=1, font=('Helvetica', 11), style='Custom.TCombobox')  # Adjust font size as needed
 start_combo2.place(x=319, y=14)  # Adjust the coordinates for indentation
+
+# Create the third Dropdownbox (placed 100px to the right of the second one)
+start_combo3 = ttk.Combobox(root, values=RateSel, width=9, height=1, font=('Helvetica', 11), style='Custom.TCombobox')  # Adjust font size as needed
+start_combo3.place(x=504, y=14)  # Adjust the coordinates for indentation
 
 # Run the Tkinter event loop
 root.mainloop()
