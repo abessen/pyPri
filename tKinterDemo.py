8b8193e3c289c7fc003c412a3667775b80785d95
Alan Bessen <abessen@quarryvision.com>
2024-03-26 10:51:57 -0500

    new commit

--- tKinterDemo.py
+++ tKinterDemo.py
@@ -6,7 +6,7 @@ root = tk.Tk()
 root.title("Display Image")
 
 # Load the image
-image_path = r"C:\pypri\pytinker\ColLSToday2.jpg"
+image_path = r"C:\pyPri\ColLSToday2.jpg"
 image = Image.open(image_path)
 image.thumbnail((1294, 1047))  # Resize the image as needed
 
