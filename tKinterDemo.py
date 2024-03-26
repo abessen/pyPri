efb1fa0efbce91de7b203b9baefba15852d8653e
Alan Bessen <abessen@quarryvision.com>
2024-03-26 11:48:00 -0500

    new commit

--- tKinterDemo.py
+++ tKinterDemo.py
@@ -9,6 +9,12 @@ root.title("Display Image")
 # Set background color to black
 root.configure(background='black')
 
+# Define custom style for the dropdown
+style = ttk.Style()
+style.theme_use('clam')  # Use the 'clam' theme for the style
+style.configure('Custom.TCombobox', background='light green')  # Set background color to light green
+
+
 HrSel = ("12:01 AM", "12:30 AM", "1:00 AM", "1:30 AM", "2:00 AM", "2:30 AM", "3:00 AM", "3:30 AM", "4:00 AM",
          "4:30 AM", "5:00 AM", "5:30 AM", "6:00 AM", "6:30 AM", "7:00 AM", "7:30 AM", "8:00 AM", "8:30 AM", "9:00 AM",
          "9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM", "12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM",
